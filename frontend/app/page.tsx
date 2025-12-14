89a18ba47d69075d7086a42767e2a930b337bab3
apccmfamador <cmfamador@student.apc.edu.ph>
2025-12-14 11:09:29 +0000

    Initialized frontend css and faci landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <Image
           className="dark:invert"
           src="/next.svg"
-          alt="Next.js logo"
+          alt="Next.js logo" 
           width={100}
           height={20}
           priority
@@ -38,7 +38,7 @@ export default function Home() {
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            target="_blank" 
             rel="noopener noreferrer"
           >
             <Image
